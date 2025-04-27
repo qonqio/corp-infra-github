9e18b8bc4fb42545abfcaca6b3dd46c09d8a8091
Mark Tinderholt <markti@outlook.com>
2025-04-26 21:11:59 -0400

    Managed by Terraform

---
+++ src/terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "application_name" {
+  type = string
+}
+variable "environment_name" {
+  type = string
+}
+variable "primary_location" {
+  type = string
+}
+variable "github_token" {
+  type = string
+}
+variable "address_space" {
+  type = string
+}
+variable "container_name" {
+  type    = string
+  default = "github-runner"
+}
+variable "additional_tags" {
+  type = map(string)
+  default = {}
+}
