6393a56ed9abac22fe29714e46f2b51ea0eda40f
Mark Tinderholt <markti@outlook.com>
2025-04-26 21:02:54 -0400

    Managed by Terraform

--- src/terraform/container-app.tf
+++ src/terraform/container-app.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_app_environment" "main" {
 
-  name                       = "cae-${var.name}"
+  name                       = "cae-${var.application_name}-${var.environment_name}"
   location                   = azurerm_resource_group.main.location
   resource_group_name        = azurerm_resource_group.main.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
