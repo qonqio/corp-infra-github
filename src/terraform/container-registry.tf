65a7ecaa7a1ff0df9291304fcbf19b7628ed4ea4
Mark Tinderholt <markti@outlook.com>
2025-04-27 15:25:19 -0400

    Managed by Terraform

--- src/terraform/container-registry.tf
+++ src/terraform/container-registry.tf
@@ -1,3 +1,7 @@
+locals {
+  additional_region_map = { for idx, region in var.additional_regions : idx => region }
+}
+
 resource "azurerm_container_registry" "main" {
 
   name                          = "cr${var.application_name}${var.environment_name}"
@@ -7,5 +11,14 @@ resource "azurerm_container_registry" "main" {
   zone_redundancy_enabled       = true
   admin_enabled                 = true
   public_network_access_enabled = true
+
+  dynamic "georeplications" {
+    for_each = local.additional_region_map
+    content {
+      location                = georeplications.value
+      zone_redundancy_enabled = true
+      tags                    = {}
+    }
+  }
 
 }
